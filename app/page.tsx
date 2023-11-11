337edaa6552eb30c02c41b845cd41601f4810d03
benthecoder <benthecoder07@gmail.com>
2023-11-11 01:29:11 -0600

    Created landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,40 @@
-"use client";
+'use client';
 
-import axios from "axios";
-import { useState } from "react";
-import { GITHUB_API_URL } from "@/constants/constant";
-import { Select, Input, SelectItem, Button } from "@nextui-org/react";
-import { GitHubRepo } from "@/types/repo";
-import { compareDesc } from "date-fns";
+import type { NextPage } from 'next';
+import React from 'react';
+import { useRouter } from 'next/navigation'; // Adjust import for useRouter
+import { Button } from '@nextui-org/react';
+import Nav from './components/navbar/navbar'; // Ensure the path is correct
 
-export default function Page() {
-  const [repos, setRepos] = useState<GitHubRepo[]>([]);
-  const [username, setUsername] = useState<string>("");
-  const [values, setValues] = useState<Set<string>>(new Set());
+const Page: NextPage = () => {
+  const router = useRouter();
 
-  const getRepos = async (username: string) => {
-    try {
-      const response = await axios.get(
-        `${GITHUB_API_URL}/users/${username}/repos`
-      );
-      const repos: GitHubRepo[] = response.data;
-      const sortedRepo = repos.sort((a, b) =>
-        compareDesc(new Date(a.updated_at), new Date(b.updated_at))
-      );
-
-      setRepos(sortedRepo);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    getRepos(username);
+  const handleGetStartedClick = () => {
+    router.push('/onboard');
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="flex space-x-2">
-        <Input
-          type="text"
-          value={username}
-          label="Username"
-          className="w-32"
-          variant="bordered"
-          onValueChange={setUsername}
-        />
-        <Button type="submit" variant="solid">
-          Search
+      <Nav />
+      <div className='w-screen min-h-screen p-4  flex flex-col items-center justify-center'>
+        <h1 className='text-3xl sm:text-5xl font-bold text-white-800 mb-4 text-center'>
+          Welcome to Narrative.AI
+        </h1>
+        <p className='text-md sm:text-lg text-gray-600 mb-6 text-center'>
+          Using AI to craft your life narrative
+        </p>
+        <Button
+          auto
+          shadow
+          size='lg'
+          onClick={handleGetStartedClick}
+          className='bg-blue-500 hover:bg-blue-600 text-white'
+        >
+          Get Started
         </Button>
-      </form>
-      <Select
-        isRequired
-        size="lg"
-        radius="md"
-        label="Github Repo"
-        placeholder="Select a user"
-        selectionMode="multiple"
-        selectedKeys={values}
-        className="max-w-xs"
-        onSelectionChange={setValues}
-      >
-        {repos.map((repo) => (
-          <SelectItem key={repo.name} textValue={repo.name}>
-            {repo.name}
-          </SelectItem>
-        ))}
-      </Select>
-      <p className="text-small text-default-500">
-        Selected: {Array.from(values).join(", ")}
-      </p>
-    </
+      </div>
+    </>
   );
-}
+};
+
+export default Page;
