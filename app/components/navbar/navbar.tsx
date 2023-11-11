970f8ed737cc07390943c20430abe404cf07fdb4
benthecoder <benthecoder07@gmail.com>
2023-11-11 01:29:25 -0600

    Add navbar component

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from '@nextui-org/react';
+
+const AcmeLogo = () => (
+  <svg fill='none' height='36' viewBox='0 0 32 32' width='36'>
+    <path
+      clipRule='evenodd'
+      d='M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z'
+      fill='currentColor'
+      fillRule='evenodd'
+    />
+  </svg>
+);
+
+export default function Nav() {
+  return (
+    <Navbar isBordered>
+      <NavbarBrand>
+        <Link href='/' className='text-white-100'>
+          <AcmeLogo />
+          <p className='font-bold text-inherit'>ACME</p>
+        </Link>
+      </NavbarBrand>
+      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+        <NavbarItem>
+          <Link color='foreground' href='#'>
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href='#' aria-current='page'>
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color='foreground' href='#'>
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+        <NavbarItem>
+          <Button as={Link} color='primary' href='#' variant='flat'>
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
