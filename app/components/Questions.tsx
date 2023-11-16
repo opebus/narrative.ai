00109798b859f6746deab1bf023565780ee6b1d7
benthecoder <benthecoder07@gmail.com>
2023-11-16 00:33:40 -0600

    add questions to questions page

--- app/components/Questions.tsx
+++ app/components/Questions.tsx
@@ -5,23 +5,24 @@ export default function Questions() {
   return (
     <div className='flex flex-col items-center space-y-6 w-full px-10'>
       <Textarea
-        label='What is ____?'
+        label='Why are you doing a MS/Ph.D.?'
         labelPlacement='outside'
-        placeholder='Answer in 3-5 sentences'
+        placeholder='Why do you need it? What are your career goals?'
         size='lg'
         className='max-w-3xl'
       />
       <Textarea
-        label='What is ____?'
+        label='What research have you done?'
         labelPlacement='outside'
-        placeholder='Answer in 3-5 sentences'
+        placeholder='What was the problem you were trying to solve? Why was it important? What approaches did you try?
+        '
         size='lg'
         className='max-w-3xl'
       />
       <Textarea
-        label='What is ____?'
+        label='What is your research interest?'
         labelPlacement='outside'
-        placeholder='Answer in 3-5 sentences'
+        placeholder='What is an idea in your field that turns you on intellectually?'
         size='lg'
         className='max-w-3xl'
       />
